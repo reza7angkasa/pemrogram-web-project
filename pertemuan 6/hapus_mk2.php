245636cccf83b572faefc466428d8d99fa4ca5bb
reza7angkasa <72240159+reza7angkasa@users.noreply.github.com>
2020-10-21 22:29:50 +0700

    Update hapus_mk2.php

--- pertemuan 6/hapus_mk2.php
+++ pertemuan 6/hapus_mk2.php
@@ -4,14 +4,13 @@ include("koneksi.php");
 
 if( isset($_GET['id']) ){
 
-    // ambil id dari query string
+    
     $id = $_GET['id'];
 
-    // buat query hapus
+  
     $strsql = "DELETE FROM matakuliah WHERE id=$id";
     $runSQL = mysqli_query($conn,$strsql);
 
-    // apakah query hapus berhasil?
     if( $runSQL ){
         header('Location: listmatakuliah.php');
     } else {
